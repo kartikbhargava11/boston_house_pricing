0b050873a93eb909a6154c233a4bcf243704b5af
Kartik Bhargava <kartikbhargava1997@gmail.com>
2023-03-05 18:45:22 +0000

    frontend

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model = pickle.load(open("regmodel.pkl", "rb"))
 def home():
     return render_template("home.html")
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict_api", methods=["POST"])
 def predict_api():
     data = request.json["data"]
     s = np.array(list(data.values())).reshape(1, -1)
@@ -27,6 +27,14 @@ def predict_api():
     output = model.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+    output = model.predict(final_input)[0]
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))
 
 if __name__ == "__main__":
     app.run(debug=True)
